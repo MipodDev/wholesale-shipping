415bfc30c1a8b66d0b4f3cea15f12cbc1e65e565
Dilly212 <Dwaltersaztec@gmail.com>
2025-06-06 16:25:00 -0700

    service CRUD routes

--- server/src/routes/carrierService.route.js
+++ server/src/routes/carrierService.route.js
@@ -5,7 +5,13 @@ const {
   synchronizeAllServices,
   synchronizeOneService,
 } = require("../services/carrierService.service");
-
+const {
+  getAllServices,
+  getServiceById,
+  createService,
+  updateService,
+  deleteService,
+} = require("../controllers/carrierService.controller.js");
 const { v4: uuidv4 } = require("uuid");
 
 router.post("/synchronize", async (req, res) => {
@@ -24,6 +30,60 @@ router.post("/synchronize/:id", async (req, res) => {
 
   try {
     const response = await synchronizeOneService(req_id, service_id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
+});
+
+router.get("/", async (req, res) => {
+  const req_id = uuidv4();
+  try {
+    const response = await getAllServices(req_id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
+});
+router.get("/:id", async (req, res) => {
+  const req_id = uuidv4();
+  const service_id = req.params.id;
+
+  try {
+    const response = await getServiceById(req_id, service_id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
+});
+router.post("/", async (req, res) => {
+  const req_id = uuidv4();
+  const input = req.body;
+  try {
+    const response = await createService(req_id, input);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
+});
+router.patch("/:id", async (req, res) => {
+  const req_id = uuidv4();
+  const service_id = req.params.id;
+
+  const input = req.body;
+  try {
+    const response = await updateService(req_id, service_id, input);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
+});
+router.delete("/:id", async (req, res) => {
+  const req_id = uuidv4();
+  const service_id = req.params.id;
+
+  try {
+    const response = await deleteService(req_id, service_id);
     res.status(200).send(response);
   } catch (error) {
     res.status(405).send(error);
