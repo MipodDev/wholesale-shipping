2e62367ea58eb959c613b3fa282ae0f0383294dd
Dilly212 <Dwaltersaztec@gmail.com>
2025-06-06 14:43:31 -0700

    List Synchronization & Routes

--- server/src/routes/list.route.js
+++ server/src/routes/list.route.js
@@ -1,33 +1,93 @@
 const express = require("express");
 const router = express.Router();
 const colors = require("colors");
-const { processLists } = require("../services/list.service");
+const { v4: uuidv4 } = require("uuid");
+const {
+  synchronizeLists,
+  synchronizeOneList,
+} = require("../services/list.service");
+const {
+  createList,
+  updateList,
+  deleteList,
+  getListById,
+  getAllLists,
+} = require("../controllers/list.controller");
 
-router.post("/process", async (req, res) => {
-  res.send("Request Received to Process Lists.");
-  (async () => {
-    try {
-      await processLists();
-    } catch (error) {
-      console.error(error);
-    }
-  })();
-});
 
 router.get("/", async (req, res) => {
   const req_id = uuidv4();
+  try {
+    const response = await getAllLists(req_id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
 });
 router.get("/:id", async (req, res) => {
   const req_id = uuidv4();
+  const list_id = req.params.id;
+
+  try {
+    const response = await getListById(req_id, list_id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
 });
-router.post("/:id", async (req, res) => {
+router.post("/", async (req, res) => {
   const req_id = uuidv4();
+  const input = req.body;
+  try {
+    const response = await createList(req_id, input);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
 });
 router.patch("/:id", async (req, res) => {
   const req_id = uuidv4();
+  const list_id = req.params.id;
+
+  const input = req.body;
+  try {
+    const response = await updateList(req_id, list_id, input);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
 });
 router.delete("/:id", async (req, res) => {
   const req_id = uuidv4();
+  const list_id = req.params.id;
+
+  try {
+    const response = await deleteList(req_id, list_id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
+});
+
+router.post("/synchronize", async (req, res) => {
+  const req_id = uuidv4();
+  try {
+    const response = await synchronizeLists(req_id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
+});
+
+router.post("/synchronize/:id", async (req, res) => {
+  const req_id = uuidv4();
+  const list_id = req.params.id;
+  try {
+    const response = await synchronizeOneList(req_id, list_id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(405).send(error);
+  }
 });
 
 module.exports = router;
--- server/src/services/list.service.js
+++ server/src/services/list.service.js
@@ -1,38 +1,147 @@
 const ListData = require("../models/list.model");
 const RuleData = require("../models/rule.model");
+const productData = require("../models/product.model");
+
 const { v4: uuidv4 } = require("uuid");
 const colors = require("colors");
 
-async function processLists() {
-    
-  const lists = await ListData.find();
-  console.log("lists found:", lists.length);
-  let totalupdates = 0;
-    console.log(`Applying updates to lists...`.blue.bold);
+async function synchronizeLists(req_id) {
+  let results = {
+    req_id,
+    lists: 0,
+    updated: 0,
+    errors: [],
+  };
 
+  const lists = await ListData.find().select("id name");
+  console.log(`[${req_id}] Found Lists:`.green.bold, lists.length);
+  results.lists = lists.length;
   for (let i = 0; i < lists.length; i++) {
-    let updates_found = 0;
-
-    console.log(`Updating:`.yellow.bold, lists[i].name);
-
-    if (!lists[i].id) {
-      updates_found++;
-
-      console.log(`Adding ID to List...`.yellow);
-      lists[i].id = uuidv4();
+    try {
+      const sync = await synchronizeOneList(req_id, lists[i].id);
+      if (sync) {
+        results.updated++;
+      }
+    } catch (error) {
+      results.push(error);
     }
-    if (updates_found > 0) {
-      const saved = lists[i].save();
-      console.log(`Updated:`.green.bold, lists[i].name);
-    } else {
-      console.log(`No updates found...`.yellow);
-    }
-
-    totalupdates += updates_found;
   }
-
-  console.log(`Total Updates:`.blue.bold, totalupdates);
+  return results;
 }
+
+async function synchronizeOneList(req_id, list_id) {
+  try {
+    const existing = await ListData.findOne({ id: list_id });
+    console.log(`\n[${req_id}] Synchronizing List:`.blue.bold, existing.name);
+
+    let product_set = new Set();
+    let sku_set = new Set();
+    // Query Included Products and Add to Product Set/Skus
+    console.log(`[${req_id}] Inclusion Objects:`.blue, existing.include.length);
+    if (existing.include.length > 0) {
+      for (let i = 0; i < existing.include.length; i++) {
+        const query = existing.include[i];
+        const products = await queryProducts(req_id, query);
+        for (let j = 0; j < products.length; j++) {
+          const { product_id, title, status, variants, unique_skus } =
+            products[j];
+          if (product_set.has({id: product_id})) {
+            console.log(
+              `[${req_id}] - Product already added to list:`.yellow,
+              product_id
+            );
+          } else {
+            // console.log(
+            //   `[${req_id}] - Product added to list:`.green,
+            //   product_id
+            // );
+            
+            product_set.add({
+              id: product_id,
+              title,
+              status,
+              variants,
+            });
+            for (let sku of unique_skus) {
+              if (!sku_set.has(sku)) {
+                sku_set.add(sku);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // Query Excluded Products and Remove from Product Set/Skus
+    console.log(`[${req_id}] Exclusion Objects:`.blue, existing.exclude.length);
+
+    if (existing.exclude.length > 0) {
+      for (let i = 0; i < existing.exclude.length; i++) {
+        const query = existing.exclude[i];
+        const products = await queryProducts(req_id, query);
+        for (let j = 0; j < products.length; j++) {
+          const { product_id, unique_skus } = products[j];
+          if (product_set.has({id: product_id})) {
+            console.log(
+              `[${req_id}] - Excluding Product from List:`.red,
+              product_id
+            );
+            product_set.delete({id: product_id});
+            for (let sku of unique_skus) {
+              if (sku_set.has(sku)) {
+                sku_set.delete(sku);
+              }
+            }
+          } else {
+            console.log(
+              `[${req_id}] - Product isn't included in List:`.yellow,
+              product_id
+            );
+          }
+        }
+      }
+    }
+    const skus = Array.from(sku_set);
+    const products = Array.from(product_set);
+
+    console.log(`[${req_id}] Products Included:`.green, products.length);
+        console.log(`[${req_id}] Skus Included:`.green, skus.length);
+
+    existing.skus = skus;
+    existing.products = products;
+    const saved = await existing.save();
+    console.log(`[${req_id}] Synchronized List:`.green.bold, saved.name);
+
+  } catch (error) {
+    console.log(error);
+  }
+  return false;
+}
+
+async function queryProducts(req_id, query) {
+  const { key, value } = query;
+  console.log(
+    `[${req_id}] Executing Product Query | Type: ${key} | Value: ${value}`.yellow
+  );
+
+  let products = [];
+  switch (key) {
+    case "category":
+      products = await productData.find({ category: value });
+      break;
+    case "tag":
+      products = await productData.find({ tags: value });
+      break;
+    case "flavor":
+      console.log(`Work in Progress...`);
+      break;
+  }
+  console.log(`[${req_id}] Products retreived:`.green.bold, products.length);
+
+  return products;
+}
+
 module.exports = {
-  processLists,
+  synchronizeLists,
+  synchronizeOneList,
 };
