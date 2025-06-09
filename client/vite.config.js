a79b73a8622d9a1a740533a7b9255dd79311e0b3
MipodDev <135762643+MipodDev@users.noreply.github.com>
2025-06-09 11:34:53 -0700

    react router + tailwind

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
 // client/vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
